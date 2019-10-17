769e5b26809b15375ee1df31d370683cfd0e98ca
David.Johnson <davidt.johnson75@yahoo.com>
2019-10-17 16:45:30 -0500

    MVP and styling completed

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -2,25 +2,21 @@ import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import MovieCard from "./MovieCard";
-export default class MovieList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movies: []
-    };
-  }
 
+export default class MovieList extends Component {
   componentDidMount() {
     axios
       .get("http://localhost:5000/api/movies")
-      .then(res => this.setState({ movies: res.data }))
+      .then(res => {
+        this.props.setMovieList(res.data)
+      })
       .catch(err => console.log(err.response));
   }
 
   render() {
     return (
       <div className="movie-list">
-        {this.state.movies.map(movie => (
+        {this.props.movieList.map(movie => (
           <MovieDetails key={movie.id} movie={movie} />
         ))}
       </div>
